4a3271822af89a1ad02fc7e5a85803ad67c03ee2
pro-mohamed-hamada <pro.mohamed.hamada@gmail.com>
2024-02-26 18:22:22 +0200

    bug fix complaint replies

--- database/migrations/2024_02_21_003225_create_complaint_replays_table.php
+++ database/migrations/2024_02_21_003225_create_complaint_replays_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('complaint_replays', function (Blueprint $table) {
             $table->id();
-            $table->string('replay');
+            $table->longText('replay');
             $table->foreignIdFor(\App\Models\Complaint::class)->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
