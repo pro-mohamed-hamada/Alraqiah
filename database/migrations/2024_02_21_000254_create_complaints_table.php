6d89c50a9c605408e3c030ea50efbb689e5f7a75
pro-mohamed-hamada <pro.mohamed.hamada@gmail.com>
2024-02-26 18:14:51 +0200

    bug fix complaints

--- database/migrations/2024_02_21_000254_create_complaints_table.php
+++ database/migrations/2024_02_21_000254_create_complaints_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('complaints', function (Blueprint $table) {
             $table->id();
-            $table->string('complaint');
+            $table->longText('complaint');
             $table->enum('is_active', [ActivationStatusEnum::ACTIVE, ActivationStatusEnum::NOT_ACTIVE])->default(ActivationStatusEnum::ACTIVE);
             $table->foreignIdFor(\App\Models\Client::class)->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
