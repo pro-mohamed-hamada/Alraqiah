59156478bd56aa6b0d506c8ff554ccda644943ad
pro-mohamed-hamada <pro.mohamed.hamada@gmail.com>
2024-03-03 22:25:16 +0200

    bug fix profile image

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,7 +6,7 @@
         <ul>
                 <li>
                         <a class="user-data text-center list-group-item">
-                                <img class="user-img img-responsive img-circle img-thumbnail" src="{{Auth::user()->getFirstMediaUrl('users') ?? asset('images/default-image.jpg')}}">
+                                <img class="user-img img-responsive img-circle img-thumbnail" src="{{ empty(Auth::user()->getFirstMediaUrl('users')) ? asset('images/default-image.jpg'):Auth::user()->getFirstMediaUrl('users')}}">
                                 <h4><span>{{ Auth::user()->name }}</span></h4>
                                 <a class="text-center list-group-item active" href="{{url("/settings")}}"><span>{{ __("lang.profile") }}</span></a>
                         </a>
