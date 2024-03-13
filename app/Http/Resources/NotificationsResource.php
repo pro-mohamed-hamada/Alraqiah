98373ce4f54074a7336c0fb8c33cb7bdeeefaff7
pro-mohamed-hamada <pro.mohamed.hamada@gmail.com>
2024-03-13 16:59:03 +0200

    return created at in notifications

--- app/Http/Resources/NotificationsResource.php
+++ app/Http/Resources/NotificationsResource.php
@@ -20,6 +20,7 @@ class NotificationsResource extends JsonResource
            'title'=>$this->data['title'],
            'content'=>$this->data['content'],
            'read_at'=>$this->read_at,
+           'created_at'=>$this->created_at,
        ];
     }
 }
