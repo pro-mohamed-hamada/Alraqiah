234225b5b200b5ed04a51f709ad0222ee32beb70
pro-mohamed-hamada <pro.mohamed.hamada@gmail.com>
2024-03-17 04:30:01 +0200

    bug fix relatives seeder

--- database/seeders/RelativesSeeder.php
+++ database/seeders/RelativesSeeder.php
@@ -15,7 +15,7 @@ class RelativesSeeder extends Seeder
         $relative1 = Relative::create([
             'name'=>'relative 1',
             'gender'=>'male',
-            'national_number'=>'123944729323434',
+            'identity_number'=>'123944729323434',
             'seat_number'=>'1501',
             'city'=>'city one',
             'client_id'=>1,
@@ -23,7 +23,7 @@ class RelativesSeeder extends Seeder
         $relative2 = Relative::create([
             'name'=>'relative 2',
             'gender'=>'female',
-            'national_number'=>'74382320384374',
+            'identity_number'=>'74382320384374',
             'seat_number'=>'1502',
             'city'=>'city two',
             'client_id'=>1,
