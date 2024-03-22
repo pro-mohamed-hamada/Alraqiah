9b6819235289c641af7018eab80e002c9743ab6e
pro-mohamed-hamada <pro.mohamed.hamada@gmail.com>
2024-03-22 16:59:13 +0200

    modify cron jobs times

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command(ClientLaunchDateReminderCommand::class)->everyMinute();
+        $schedule->command(ClientLaunchDateReminderCommand::class)->daily();
     }
 
     /**
