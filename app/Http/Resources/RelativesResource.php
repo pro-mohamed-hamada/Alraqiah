fd05789ab88ef79cdd4f0fce35c7db158c76b97c
pro-mohamed-hamada <pro.mohamed.hamada@gmail.com>
2024-07-16 20:18:37 +0200

    relatives chronic disease resource api

--- app/Http/Resources/RelativesResource.php
+++ app/Http/Resources/RelativesResource.php
@@ -22,6 +22,8 @@ class RelativesResource extends JsonResource
             'seat_number'=>$this->seat_number,
             'country'=>$this->country,
             'city'=>$this->city,
+            "chronic_disease"=> $this->getRawOriginal('chronic_disease'),
+            "chronic_disease_discription"=> $this->chronic_disease_discription,
         ];
     }
 }
